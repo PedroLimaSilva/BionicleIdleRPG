9fcc53c8f6a0a1fa49d115e620b66ed5dde7dd79
Pedro Lima e Silva <pedrocaslsilva403@gmail.com>
2025-01-25 17:21:26 +0000

    "Set up GitHub Actions for deployment"

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/BionicleIdleRPG/',
   plugins: [react(), VitePWA({
     registerType: 'autoUpdate',
     injectRegister: false,
