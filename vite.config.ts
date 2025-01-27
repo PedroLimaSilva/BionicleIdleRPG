6eb1ff635c7ac64ddf9adf0bcd519f157fa6f8db
Pedro Lima e Silva <pedrocaslsilva403@gmail.com>
2025-01-27 11:38:31 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/BionicleIdleRPG/',
   plugins: [react(), VitePWA({
-    registerType: 'autoUpdate',
     injectRegister: false,
 
     pwaAssets: {
