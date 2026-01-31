20841e15034313da6ec491b6cc002b8236613910
Pedro Lima e Silva <pedrocaslsilva403@gmail.com>
2026-01-31 12:44:45 +0000

    Review Devices

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,22 +5,22 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './e2e',
-  
+
   /* Run tests in files in parallel */
   fullyParallel: true,
-  
+
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  
+
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  
+
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  
+
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI ? 'html' : 'list',
-  
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -57,9 +57,6 @@ export default defineConfig({
       use: {
         ...devices['Pixel 5'],
         viewport: { width: 851, height: 393 }, // Landscape dimensions for Pixel 5
-        isMobile: true,
-        hasTouch: true,
-        deviceScaleFactor: 2.75,
       },
     },
   ],
@@ -72,4 +69,3 @@ export default defineConfig({
     timeout: 120 * 1000,
   },
 });
-
