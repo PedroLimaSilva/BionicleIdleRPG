897a93ea03670fba525a5c7f7ef23abdfbfe51b9
Pedro Lima e Silva <pedrocaslsilva403@gmail.com>
2026-02-11 21:48:12 +0000

    Fix lint and possible crash

--- src/components/CharacterScene/Nuva/GaliNuvaModel.tsx
+++ src/components/CharacterScene/Nuva/GaliNuvaModel.tsx
@@ -2,29 +2,31 @@ import { useGLTF } from '@react-three/drei';
 import { CombatantModelHandle } from '../../../pages/Battle/CombatantModel';
 import { BaseMatoran, RecruitedCharacterData } from '../../../types/Matoran';
 import { forwardRef, useEffect, useRef } from 'react';
-import { Group } from 'three';
-import { applyStandardPlasticToObject } from '../StandardPlasticMaterial';
+import { Group, Mesh } from 'three';
+import {
+  applyStandardPlasticToObject,
+  getStandardPlasticMaterial,
+} from '../StandardPlasticMaterial';
 
 export const ToaGaliNuvaModel = forwardRef<
   CombatantModelHandle,
   {
     matoran: RecruitedCharacterData & BaseMatoran;
   }
->(({ matoran }, ref) => {
+>(({ matoran }) => {
   const group = useRef<Group>(null);
   const { nodes } = useGLTF(import.meta.env.BASE_URL + 'Toa_Nuva/gali.glb');
   useEffect(() => {
     if (group.current) applyStandardPlasticToObject(group.current);
   }, [nodes]);
 
-  // useEffect(() => {
-  //   const maskColor = matoran.maskColorOverride || matoran.colors.mask;
-  //   nodes.Masks.children.forEach((mask) => {
-  //     const mesh = mask as Mesh;
-  //     let mat = getStandardPlasticMaterial(maskColor);
-  //     mesh.material = mat;
-  //   });
-  // }, [nodes, matoran]);
+  useEffect(() => {
+    const maskColor = matoran.maskColorOverride || matoran.colors.mask;
+    // TODO: add all masks to the blender and reexport
+    const mesh = nodes.Kaukau as Mesh;
+    const mat = getStandardPlasticMaterial(maskColor);
+    mesh.material = mat;
+  }, [nodes, matoran]);
 
   return (
     <group ref={group} dispose={null}>
--- src/components/CharacterScene/Nuva/TahuNuvaModel.tsx
+++ src/components/CharacterScene/Nuva/TahuNuvaModel.tsx
@@ -2,15 +2,18 @@ import { useGLTF } from '@react-three/drei';
 import { CombatantModelHandle } from '../../../pages/Battle/CombatantModel';
 import { BaseMatoran, RecruitedCharacterData } from '../../../types/Matoran';
 import { forwardRef, useEffect, useRef } from 'react';
-import { Group } from 'three';
-import { applyStandardPlasticToObject } from '../StandardPlasticMaterial';
+import { Group, Mesh } from 'three';
+import {
+  applyStandardPlasticToObject,
+  getStandardPlasticMaterial,
+} from '../StandardPlasticMaterial';
 
 export const ToaTahuNuvaModel = forwardRef<
   CombatantModelHandle,
   {
     matoran: RecruitedCharacterData & BaseMatoran;
   }
->(({ matoran }, ref) => {
+>(({ matoran }) => {
   const group = useRef<Group>(null);
   const { nodes } = useGLTF(import.meta.env.BASE_URL + 'Toa_Nuva/tahu.glb');
 
@@ -18,14 +21,13 @@ export const ToaTahuNuvaModel = forwardRef<
     if (group.current) applyStandardPlasticToObject(group.current);
   }, [nodes]);
 
-  // useEffect(() => {
-  //   const maskColor = matoran.maskColorOverride || matoran.colors.mask;
-  //   nodes.Masks.children.forEach((mask) => {
-  //     const mesh = mask as Mesh;
-  //     let mat = getStandardPlasticMaterial(maskColor);
-  //     mesh.material = mat;
-  //   });
-  // }, [nodes, matoran]);
+  useEffect(() => {
+    const maskColor = matoran.maskColorOverride || matoran.colors.mask;
+    // TODO: add all masks to the blender and reexport
+    const mesh = nodes.Hau as Mesh;
+    const mat = getStandardPlasticMaterial(maskColor);
+    mesh.material = mat;
+  }, [nodes, matoran]);
 
   return (
     <group ref={group} dispose={null}>
