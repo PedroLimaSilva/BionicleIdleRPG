a746856e0fc7bcdb8c55e492f0bef47d186180de
Pedro Lima e Silva <pedrocaslsilva403@gmail.com>
2026-04-11 12:26:02 +0100

    Onua Review

--- src/components/CharacterScene/Nuva/OnuaNuvaModel.tsx
+++ src/components/CharacterScene/Nuva/OnuaNuvaModel.tsx
@@ -37,13 +37,7 @@ export const OnuaNuvaModel = forwardRef<
         edgeColor: '#ffffff',
         edgeStrength: 0.15,
         edgeCurvatureScale: 2,
-        excludeMaterialNames: [
-          'Glowing Eyes',
-          'Trans Dark Pink',
-          'SOLID-SILVER',
-          'Nuva Armour',
-          'Holder',
-        ],
+        excludeMaterialNames: ['Glowing Eyes', 'Brain', 'SOLID-SILVER', 'Nuva Armour', 'Holder'],
       });
     }
   }, [nodes]);
