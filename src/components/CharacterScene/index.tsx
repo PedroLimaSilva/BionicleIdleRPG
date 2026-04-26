7f7e3c8e6b0652c34f3b3b7e97319c83938be8f0
Pedro Lima e Silva <pedrocaslsilva403@gmail.com>
2026-04-26 01:26:32 +0100

    Wire Onua Mata

--- src/components/CharacterScene/index.tsx
+++ src/components/CharacterScene/index.tsx
@@ -74,7 +74,7 @@ function CharacterModel({
         case 'Toa_Kopaka':
           return <KopakaMataModel matoran={matoran} onKitMeshesAttached={onModelReady} />;
         case 'Toa_Onua':
-          return <OnuaMataModel matoran={matoran} />;
+          return <OnuaMataModel matoran={matoran} onKitMeshesAttached={onModelReady} />;
         case 'Toa_Lewa':
           return <LewaMataModel matoran={matoran} onKitMeshesAttached={onModelReady} />;
         default:
