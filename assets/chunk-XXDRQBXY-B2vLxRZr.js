6d18aa44ae22f6c65fa66f502e1857e10c613a67
PedroLimaSilva <PedroLimaSilva@users.noreply.github.com>
2026-08-29 01:46:48 +0000

    deploy: 2cfa253d8197758462d6aee48158bc1cd6c33301

---
+++ assets/chunk-XXDRQBXY-B2vLxRZr.js
@@ -0,0 +1,1 @@
+import{_ as a,j as o}from"./index-C-1VM9k5.js";var g=a((t,e)=>{let n;return e==="sandbox"&&(n=o("#i"+t)),(e==="sandbox"?o(n.nodes()[0].contentDocument.body):o("body")).select(`[id="${t}"]`)},"getDiagramElement");export{g};
--- assets/swimlanesDiagram-VR7AAH4N-oLDYarR9.js
+++
@@ -1,8 +0,0 @@
-import{c as e,s as r}from"./flowDiagram-HODETNUW-DWm-TmI3.js";import{_ as a}from"./index-NaKNGB4X.js";import"./chunk-5VM5RSS4-CGlzYlPN.js";import"./chunk-XXDRQBXY-sliJVmoI.js";import"./chunk-POPQ4Y6H-DQGxzi20.js";import"./chunk-F27PBJKO-Do_dl8Yg.js";import"./channel-Ch3uFsk7.js";var o=a(t=>`${r(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
