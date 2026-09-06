30f395d19887bd3717d544fc4fa3f597b58e72f3
PedroLimaSilva <PedroLimaSilva@users.noreply.github.com>
2026-09-06 19:50:54 +0000

    deploy: 267a29bc09415b68e6bdc8f618a4c1606b02e6d5

--- assets/swimlanesDiagram-VR7AAH4N-DN5PVy5c.js
+++
@@ -1,8 +0,0 @@
-import{c as e,s as r}from"./flowDiagram-HODETNUW-BsvnPI-j.js";import{_ as a}from"./index-CSDXgETO.js";import"./chunk-5VM5RSS4-B36q4C7r.js";import"./chunk-XXDRQBXY-BPK3Ufh3.js";import"./chunk-POPQ4Y6H-DgLzNqOK.js";import"./chunk-F27PBJKO-CzK2TvQc.js";import"./channel-CIJGdNWM.js";var o=a(t=>`${r(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
