5428755db60f4dbc9a74e35e9f7c096fae367493
PedroLimaSilva <PedroLimaSilva@users.noreply.github.com>
2026-09-13 13:54:39 +0000

    deploy: 85ad9aec63e755af4d730b35cb63524c4a9ce8b7

---
+++ assets/chunk-5VM5RSS4-BanfZ294.js
@@ -0,0 +1,17 @@
+!function(){try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:{};e._posthogReleaseId=e._posthogReleaseId||"01a09b0b-e90e-0000-f2f6-5ee4638d3cc0";var n=(new e.Error).stack;n&&(e._posthogChunkIds=e._posthogChunkIds||{},e._posthogChunkIds[n]="6108b81f-1f74-5e20-96c2-c2bbb91dc573")}catch(e){}}();import{_ as e}from"./index-BnYJAZbj.js";var l=e(()=>`
+  /* Font Awesome icon styling - consolidated */
+  .label-icon {
+    display: inline-block;
+    height: 1em;
+    overflow: visible;
+    vertical-align: -0.125em;
+  }
+  
+  .node .label-icon path {
+    fill: currentColor;
+    stroke: revert;
+    stroke-width: revert;
+  }
+`,"getIconStyles");export{l as g};
+
+//# chunkId=6108b81f-1f74-5e20-96c2-c2bbb91dc573
