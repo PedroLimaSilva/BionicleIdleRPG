f8766e75cc089be590c41380c6a911a9c0562b33
PedroLimaSilva <PedroLimaSilva@users.noreply.github.com>
2026-09-17 21:33:20 +0000

    deploy: c409a47f9318c0c3789fe0d4093c00ab3a255fb5

---
+++ assets/swimlanesDiagram-VR7AAH4N-CVoyLHo0.js
@@ -0,0 +1,10 @@
+!function(){try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:{};e._posthogReleaseId=e._posthogReleaseId||"01a0b149-40ee-0000-8f65-014fa7944d1e";var n=(new e.Error).stack;n&&(e._posthogChunkIds=e._posthogChunkIds||{},e._posthogChunkIds[n]="5f28a8d5-a097-5643-8029-68b760d413f9")}catch(e){}}();import{c as e,s as r}from"./flowDiagram-HODETNUW-BCd_kA8q.js";import{_ as a}from"./index-C2RxjLJx.js";import"./chunk-5VM5RSS4-DNOTi88A.js";import"./chunk-XXDRQBXY-BsvZrZMM.js";import"./chunk-POPQ4Y6H-bIBLhL9e.js";import"./chunk-F27PBJKO-bu2ySbPR.js";import"./channel-CMsY3zLp.js";var o=a(t=>`${r(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
+
+//# chunkId=5f28a8d5-a097-5643-8029-68b760d413f9
